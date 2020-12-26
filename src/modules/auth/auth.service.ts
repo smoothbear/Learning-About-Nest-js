5f222bbefeade0e24a82cfb23a95a4f9d40ff703
kjbin0420 <kjbin0420@naver.com>
2020-12-26 18:02:29 +0900

    [ADD] Password verification logic

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { UserRepository } from "../user/user.repository";
 import { LoginRequest } from "./payload/request/login.request";
+import { TokenResponse } from "./payload/response/token.response";
 
 @Injectable()
 export class AuthService {
-    async login(request: LoginRequest) {
+    constructor(private readonly userRepository: UserRepository) {}
 
+    async login(request: LoginRequest): Promise<TokenResponse> {
+        try {
+            const user = await this.userRepository.findOneOrFail({
+                where: {
+                    id: request.id
+                }
+            });
+
+            if (user.password === request.password) {
+                return ({
+                    accessToken: ,
+                    refreshToken: ,
+                    expireTime: ,
+                });
+            } else {
+                throw new HttpException('로그인 정보가 일치하지 않습니다.', HttpStatus.UNAUTHORIZED);
+            }
+        } catch {
+            throw new HttpException('아이디를 찾을 수 없습니다.', HttpStatus.NOT_FOUND);
+        }
     }
 }
