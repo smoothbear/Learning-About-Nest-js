3316a12a07c3e0e97c9a2f77b33eb424addc34ca
kjbin0420 <kjbin0420@naver.com>
2020-12-27 22:55:37 +0900

    [PATCH] Fix unused imports 😀

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, Post } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { CreateUserRequest } from "./payload/request/create-user.request";
 import { UserService } from "./user.service";
 
